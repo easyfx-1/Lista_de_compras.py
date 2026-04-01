082d104eb21c3a868f22eaab567ce38ffc871295
Rafael <rafaelforex307@gmail.com>
2026-04-01 17:45:05 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -1,24 +1,35 @@
 lista = []
 
 while True:
+    print('\ninserir, listar, deletar, sair\n')
+    
     pergunta = input('O que deseja fazer em sua lista? ').lower()
 
-    if pergunta == 'inserir' or pergunta == 'i':
+    if pergunta == 'inserir':
         inserir = input('Qual item deseja inserir? ').lower()
         lista.append(inserir)
         
-    elif pergunta ==  'listar' or pergunta == 'l':
-        for indice, item in enumerate(lista):
-            print(indice, item)
+    elif pergunta ==  'listar':
+        if not lista:
+            print('Lista vazia')
+        else:
+            for indice, item in enumerate(lista):
+                print(indice, item)
     
-    elif pergunta == 'deletar' or pergunta == 'del':
-        for indice, item in enumerate(lista):
-            print(indice, item)
-        try:    
-            deletar = int(input('Qual indice deseja deletar? '))
-            lista.pop(deletar)
-        except:
-            print('Indice inválido')
+    elif pergunta == 'deletar':
+        if not lista:
+            print('Lista vazia')
+        else:
+            for indice, item in enumerate(lista):
+                print(indice, item)
+            try:
+                deletar = int(input('Qual indice deseja deletar? '))
+                lista.pop(deletar)
+            except:
+                print('Indice inválido')
+    elif pergunta == 'sair':
+        print('Encerrando programa...')
+        break
         
          
         
